450a656d2c48ef160ab2e9c5cb91526726db05c4
carolinavasquez <deysicarolina@unitec.com>
2014-03-18 17:11:57 -0600

    Update vasquez2.java

--- src/vasquez2.java
+++ src/vasquez2.java
@@ -5,62 +5,46 @@ public class vasquez2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	Scanner mi_scanner = new Scanner(System.in);
-		
-	    int nota1;
-	    int nota2;
-	    int nota3;
-	    int nota4;
-	    System.out.println("nombre del Alumno");
-		Scanner mi_scannera = new Scanner (System.in);
-		String nombre1=mi_scannera.next();
-	    System.out.println("nota1: ");
-	    nota1 = mi_scanner.nextInt();
-	    System.out.println("nota2: ");
-	    nota2 = mi_scanner.nextInt();
-	    System.out.println("nota3: ");
-	    nota3 = mi_scanner.nextInt();
-	    nota4 = ((nota1+nota2+nota3)/(3));
-	    System.out.println("El promedio es: " + nota4);
-
-	    if(nota4>=60){
-	        System.out.println("El Alumno Aprobo");
-	    }else
-	        System.out.println("El Alumno Reprobo");
-
-	    System.out.println("nombre del Alumno");
-		Scanner mi_scannerv = new Scanner (System.in);
-		String nombre2=mi_scannera.next();
-	    System.out.println(" nota1: ");
-	    nota1 = mi_scanner.nextInt();
-	    System.out.println("nota2: ");
-	    nota2 = mi_scanner.nextInt();
-	    System.out.println("nota3: ");
-	    nota3 = mi_scanner.nextInt();
-	    nota4 = ((nota1+nota2+nota3)/(3));
-	    System.out.println("El promedio es: " + nota4);
-
-	    if(nota4>=60){
-	        System.out.println("El Alumno Aprobo");
-	    }else
-	        System.out.println("El Alumno Reprobo");
-
-	    System.out.println("nombre del Alumno");
-		Scanner mi_scannerc = new Scanner (System.in);
-		String nombre3=mi_scannera.next();
-	    System.out.println("nota1: ");
-	    nota1 = mi_scanner.nextInt();
-	    System.out.println("nota2: ");
-	    nota2 = mi_scanner.nextInt();
-	    System.out.println("nota3: ");
-	    nota3 = mi_scanner.nextInt();
-	    nota4 = ((nota1+nota2+nota3)/(3));
-	    System.out.println("El promedio es: " + nota4);
-
-	    if(nota4>=60){
-	        System.out.println("El Alumno Aprobo");
-	    }else
-	        System.out.println("El Alumno Reprobo");
-	}
-
-	}
+   Scanner teclado=new Scanner(System.in);
+	        int nota1,nota2,nota3;
+	        System.out.print("alumno1:");
+	        System.out.print("Ingrese primer nota:");
+	        nota1=teclado.nextInt();
+	        System.out.print("Ingrese segunda nota:");
+	        nota2=teclado.nextInt();
+	        System.out.print("Ingrese tercer nota:");
+	        nota3=teclado.nextInt();
+	        int promedio=(nota1 + nota2 + nota3) / 3;
+	        if (promedio>=60) {
+	            System.out.print("aprobado");    
+	        } else 
+	                System.out.print("Reprobado");
+	                System.out.print("alumno2:");
+	    	        System.out.print("Ingrese primer nota:");
+	    	        nota1=teclado.nextInt();
+	    	        System.out.print("Ingrese segunda nota:");
+	    	        nota2=teclado.nextInt();
+	    	        System.out.print("Ingrese tercer nota:");
+	    	        nota3=teclado.nextInt();
+	    	        int promedio1=(nota1 + nota2 + nota3) / 3;
+	    	        if (promedio1>=60) {
+	    	            System.out.print("aprobado");    
+	    	        } else 
+	    	           
+	    	                System.out.print("Reprobado");          
+	    	                System.out.print("alumno3:");
+	    	    	        System.out.print("Ingrese primer nota:");
+	    	    	        nota1=teclado.nextInt();
+	    	    	        System.out.print("Ingrese segunda nota:");
+	    	    	        nota2=teclado.nextInt();
+	    	    	        System.out.print("Ingrese tercer nota:");
+	    	    	        nota3=teclado.nextInt();
+	    	    	        int promedio2=(nota1 + nota2 + nota3) / 3;
+	    	    	        if (promedio2>=60) {
+	    	    	            System.out.print("aprobado");    
+	    	    	        } else 
+	    	    	           
+	    	    	                System.out.print("Reprobado");          
+	    	                
+	            }
+	        }
